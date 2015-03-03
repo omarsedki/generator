fce4b231cd3a7a1d12cfe48fe1ac3b9e9d8c11c7
omarsedki <noreply>
2015-03-03 14:10:00 +0000

    Add component menu

---
+++ ui/menu.reel/menu.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/menu.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Menu
+ * @extends Component
+ */
+exports.Menu = Component.specialize(/** @lends Menu# */ {
+    constructor: {
+        value: function Menu() {
+            this.super();
+        }
+    }
+});
